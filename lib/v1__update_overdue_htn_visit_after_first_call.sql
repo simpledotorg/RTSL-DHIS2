5722e0a5d8149a7be5f0ed7dd52553d7b462da4a
Priyanga P Kini <priyangapkini99@gmail.com>
2024-04-15 19:48:03 +0530

    Format document

--- lib/v1__update_overdue_htn_visit_after_first_call.sql
+++ lib/v1__update_overdue_htn_visit_after_first_call.sql
@@ -5,7 +5,7 @@ htn_diabetes_program_stage_uid TEXT := 'anb2cjLx3WM';
 
 calling_report_program_stage_uid TEXT := 'W7BCOaSquMd';
 
-firstcalldate_data_element_uid TEXT := 'XXX';
+first_call_date_data_element_uid TEXT := 'Y6WYj6bbgeV';
 
 latest_overdue_event_data JSONB;
 
@@ -17,50 +17,48 @@ BEGIN
     IF NEW.programstageid = (SELECT programstageid FROM programstage WHERE uid = calling_report_program_stage_uid) THEN
         -- Query the programstageinstance table to find the latest overdue event of the HTN & diabetes program stage
 
-SELECT eventdatavalues, programstageinstanceid INTO latest_overdue_event_data, latest_overdue_event_id
-FROM programstageinstance
-WHERE programinstanceid = NEW.programinstanceid
-  AND programstageid = (SELECT programstageid FROM programstage WHERE uid = htn_diabetes_program_stage_uid)
-  AND status = 'SCHEDULE'
-  AND duedate < NEW.executiondate
-ORDER BY duedate DESC
-    LIMIT 1;
+            SELECT eventdatavalues, programstageinstanceid INTO latest_overdue_event_data, latest_overdue_event_id
+            FROM programstageinstance
+            WHERE programinstanceid = NEW.programinstanceid
+              AND programstageid = (SELECT programstageid FROM programstage WHERE uid = htn_diabetes_program_stage_uid)
+              AND status = 'SCHEDULE'
+              AND duedate < NEW.executiondate
+            ORDER BY duedate DESC
+                LIMIT 1;
 
-IF (latest_overdue_event_data IS NULL)
-            OR NOT (
-                latest_overdue_event_data ? firstcalldate_data_element_uid
-            ) THEN -- Update eventdatavalues in HTN & diabetes event program stage
-            RAISE NOTICE 'Adding first call details....';
+            IF (latest_overdue_event_data IS NULL) OR NOT ( latest_overdue_event_data ? first_call_date_data_element_uid) THEN
+                -- Update eventdatavalues in HTN & diabetes event program stage
+                RAISE NOTICE 'Adding first call details....';
 
-            latest_overdue_event_data = latest_overdue_event_data || NEW.eventdatavalues || jsonb_build_object(
-                firstcalldate_data_element_uid,
-                jsonb_build_object(
-                    'value',
-                    NEW.executiondate,
-                    'created',
-                    NEW.created,
-                    'lastUpdated',
-                    NEW.lastUpdated,
-                    'providedElsewhere',
-                    false
-                )
-            );
+                latest_overdue_event_data = latest_overdue_event_data || NEW.eventdatavalues || jsonb_build_object(
+                    first_call_date_data_element_uid,
+                    jsonb_build_object(
+                        'value',
+                        NEW.executiondate,
+                        'created',
+                        NEW.created,
+                        'lastUpdated',
+                        NEW.lastUpdated,
+                        'providedElsewhere',
+                        false
+                    )
+                );
 
-UPDATE
-    programstageinstance
-SET
-    eventdatavalues = latest_overdue_event_data
-WHERE
-        programstageinstanceid = latest_overdue_event_id;
+                UPDATE
+                    programstageinstance
+                SET
+                    eventdatavalues = latest_overdue_event_data
+                WHERE
+                        programstageinstanceid = latest_overdue_event_id;
 
-RAISE NOTICE 'Updated event deatils.....';
+                RAISE NOTICE 'Updated event deatils.....';
 
-ELSE RAISE NOTICE 'Ignore other calls';
+            ELSE RAISE NOTICE 'Ignore other calls';
 
-END IF;
-END IF;
+            END IF;
+    END IF;
 
-RETURN NEW;
+    RETURN NEW;
 
 END;
 
