b31d1d3d0a34973c4cc5a997ae0f6fb8867be24d
Priyanga P Kini <priyangapkini99@gmail.com>
2024-04-16 11:47:48 +0530

    Add migration file to create the trigger

---
+++ db/data/20240416_add_trigger_on_program_stage_instance.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require_relative '../../config/db_connection'
+
+class AddTriggerOnProgramStageInstance < ActiveRecord::Migration[7.0]
+
+  def up
+    execute <<-SQL
+    CREATE OR REPLACE FUNCTION update_htn_visits_after_first_call_trigger()
+    RETURNS TRIGGER AS $$
+    DECLARE
+    htn_diabetes_program_stage_uid TEXT := 'anb2cjLx3WM';
+
+    calling_report_program_stage_uid TEXT := 'W7BCOaSquMd';
+
+    first_call_date_data_element_uid TEXT := 'Y6WYj6bbgeV';
+
+    latest_overdue_event_data JSONB;
+
+    latest_overdue_event_id BIGINT;
+    BEGIN
+      -- Check if the newly inserted row corresponds to the calling report program stage
+      RAISE NOTICE 'Trigger invoked....';
+
+      IF NEW.programstageid = (SELECT programstageid FROM programstage WHERE uid = calling_report_program_stage_uid) THEN
+      -- Query the programstageinstance table to find the latest overdue event of the HTN & diabetes program stage
+
+      SELECT eventdatavalues, programstageinstanceid INTO latest_overdue_event_data, latest_overdue_event_id
+      FROM programstageinstance
+      WHERE programinstanceid = NEW.programinstanceid
+      AND programstageid = (SELECT programstageid FROM programstage WHERE uid = htn_diabetes_program_stage_uid)
+      AND deleted = 'f'
+      AND status = 'SCHEDULE'
+      AND duedate < NEW.executiondate
+      ORDER BY duedate DESC
+      LIMIT 1;
+
+      IF (latest_overdue_event_data IS NULL) OR NOT ( latest_overdue_event_data ? first_call_date_data_element_uid) THEN
+      -- Update eventdatavalues in HTN & diabetes event program stage
+      RAISE NOTICE 'Adding first call details....';
+
+      latest_overdue_event_data = latest_overdue_event_data || NEW.eventdatavalues || jsonb_build_object(
+        first_call_date_data_element_uid,
+        jsonb_build_object(
+          'value',
+          NEW.executiondate,
+          'created',
+          NEW.created,
+          'lastUpdated',
+          NEW.lastUpdated,
+          'providedElsewhere',
+          false
+        )
+      );
+
+      UPDATE
+      programstageinstance
+      SET
+      eventdatavalues = latest_overdue_event_data
+      WHERE
+      programstageinstanceid = latest_overdue_event_id;
+
+      RAISE NOTICE 'Updated event deatils.....';
+
+      ELSE RAISE NOTICE 'Ignore other calls';
+
+      END IF;
+      END IF;
+
+      RETURN NEW;
+
+      END;
+
+      $$ LANGUAGE plpgsql;
+
+      CREATE OR REPLACE TRIGGER after_insert_calling_report_programstageinstance
+      AFTER INSERT ON programstageinstance
+      FOR EACH ROW
+      EXECUTE FUNCTION update_htn_visits_after_first_call_trigger();
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP TRIGGER after_insert_calling_report_programstageinstance ON programstageinstance;
+      DROP FUNCTION update_htn_visits_after_first_call_trigger();
+    SQL
+  end
+end
+
+# Run the migration
+
