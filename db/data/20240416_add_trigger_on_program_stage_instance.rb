2b5454775d5c74a5319b8bd8ee6eefa584ce1f8f
Priyanga P Kini <priyangapkini99@gmail.com>
2024-05-27 20:40:17 +0530

    Add update patient status function in the down script

--- db/data/20240416_add_trigger_on_program_stage_instance.rb
+++ db/data/20240416_add_trigger_on_program_stage_instance.rb
@@ -12,6 +12,7 @@ class AddTriggerOnProgramStageInstance < ActiveRecord::Migration[7.0]
     execute <<-SQL
       DROP TRIGGER after_insert_calling_report_programstageinstance ON programstageinstance;
       DROP FUNCTION update_htn_visit_after_first_call_trigger();
+      DROP FUNCTION update_patient_status(program_instance_id BIGINT, remove_reason TEXT);
     SQL
   end
 end
